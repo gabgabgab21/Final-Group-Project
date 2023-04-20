5d8c48bf304903ce662ad04f44ced8a1a785a940
yimewang <qwerttt7411@gmail.com>
2023-04-20 15:16:16 -0400

    vyu

--- Coin/Coin.gd
+++ Coin/Coin.gd
@@ -4,5 +4,8 @@ var score = 10
 
 func _on_Coin_body_entered(body):
 	if body.name == "Player":
+<<<<<<< Updated upstream
 		Global.update_score(score)
+=======
+>>>>>>> Stashed changes
 		queue_free()
