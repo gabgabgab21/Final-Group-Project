d2b6d01581e8964254fe9d05279f4825ff8f797c
yimewang <qwerttt7411@gmail.com>
2023-04-20 15:20:54 -0400

    Revert "vyu"
    This reverts commit 5d8c48bf304903ce662ad04f44ced8a1a785a940.

--- Coin/Coin.gd
+++ Coin/Coin.gd
@@ -4,8 +4,5 @@ var score = 10
 
 func _on_Coin_body_entered(body):
 	if body.name == "Player":
-<<<<<<< Updated upstream
 		Global.update_score(score)
-=======
->>>>>>> Stashed changes
 		queue_free()
